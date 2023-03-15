2e9a9421f191784661a0ec5dce02619d3d85516c
Dio <Dio@DIO>
2023-03-16 01:17:45 +0700

    Добавьте файлы проекта.

---
+++ Mashina/Program.cs
@@ -0,0 +1,37 @@
+﻿using Mashina;
+
+Car[] array = new Car[0];
+int n = 0;
+bool a = true;
+while (a)
+{
+    Console.WriteLine("\n Меню: \n 1 - Добавить машину \n 2 - Поиск по базе \n 3 - Показать все машины в базе \n 4 - Колличество машин на стоянке \n 5 - Выход из программы");
+    ConsoleKey key = Console.ReadKey().Key;
+    Console.WriteLine();
+    switch (key)
+    {
+        case ConsoleKey.D1:
+            Komandei.AddCar(ref array, n);
+            n++;
+            break;
+        case ConsoleKey.D2:
+            Car.SearchCar(ref array);
+            break;
+        case ConsoleKey.D3:
+            Komandei.PrintInfo(ref array);
+            break;
+        case ConsoleKey.D4:
+            Console.WriteLine("Машин на стоянке:");
+            Car.SearchCarInfo(ref array);
+            break;
+        
+        case ConsoleKey.D5:
+            Console.WriteLine("Выход из программы");
+            Environment.Exit(0);
+            break;
+
+        default:
+            Console.WriteLine("Вы ввели неверную клавишу");
+            break;
+    }
+}
